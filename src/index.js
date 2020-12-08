d4b7855ac20bd4792176757a8c4ecaaa717c13eb
pete3249 <katelynrpeterson16@gmail.com>
2020-12-07 21:59:12 -0500

    commit tree 9c864b2eaf4a257417caa227a40392e182829f0e

--- src/index.js
+++ src/index.js
@@ -12,4 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(toys => {
+      addAndysToys(toys);
+
+    })
 });
+
+function addAndysToys(toys) {
+  toys.forEach(element => {
+    const toyCollection = document.querySelector("#toy-collection");
+    const newToyDiv = document.createElement('div');
+    const newH2 = document.createElement('h2');
+    const newImg = document.createElement('img');
+    const newP = document.createElement('p');
+    const newButton = document.createElement('button');
+
+    toyCollection.appendChild(newToyDiv);
+    newToyDiv.className = "card";
+    newH2.innerHTML = element.name;
+    newToyDiv.appendChild(newH2);
+    newImg.src = element.image;
+    newImg.className = "toy-avatar";
+    newToyDiv.appendChild(newImg);
+    newP.innerHTML = "1";
+    newToyDiv.appendChild(newP);
+    newButton.className = "like-btn";
+    newToyDiv.appendChild(newButton);
+  })
+}
